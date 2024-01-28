7bde9ec986c1bac3c304ff558788ee73af0aff94
c-demir <112612358+c-demir@users.noreply.github.com>
2024-01-28 21:10:59 +0300

    a new folder is created for boot

---
+++ boot_part/Debug/sources.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+CYCLO_FILES := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+MAP_FILES := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+App \
+Core/Src \
+Core/Startup \
+Drivers/STM32F4xx_HAL_Driver/Src \
+Library/FatFS \
+Library/STM32_USB_Device_Library/Core/Src \
+Loader/ARMCM4_STM32F4/GCC \
+Loader/ARMCM4_STM32F4 \
+Loader \
+Loader/third_party/fatfs/src \
+Loader/third_party/fatfs/src/option \
+
